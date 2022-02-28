fc03aa953b06e18c19ad01ac7763546195f85cf0
nabsternab <hayounn.etd@gmail.com>
2022-02-28 17:17:07 +0100

    add js code and functionnalty

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector('.mobile-menu-btn');
 let ham = document.querySelector('.ham');
 let menu = document.querySelector('.mobile-menu');
+let menuTag = document.querySelectorAll('.mobile-menu a');
 
 btn.addEventListener('click', function () {
   menu.classList.toggle('none');
@@ -9,3 +10,10 @@ btn.addEventListener('click', function () {
 ham.addEventListener('click', function () {
   menu.classList.toggle('none');
 });
+
+menuTag.forEach(elem => {
+  elem.addEventListener('click', function() {
+    menu.classList.add('none');
+  })
+})
+
