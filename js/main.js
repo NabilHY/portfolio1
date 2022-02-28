f38a97eccab99df5ccee8ebfd67a3d70bf1ab4b5
nabsternab <hayounn.etd@gmail.com>
2022-02-28 19:58:18 +0100

    fix linters

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ const ham = document.querySelector('.ham');
 const menu = document.querySelector('.mobile-menu');
 const menuTag = document.querySelectorAll('.mobile-menu a');
 
-btn.addEventListener('click', function (elem) {
+btn.addEventListener('click', function () {
   menu.classList.toggle('none');
 });
 
-ham.addEventListener('click', function (elem) {
+ham.addEventListener('click', function () {
   menu.classList.toggle('none');
 });
 
-menuTag.forEach(elem => {
-  elem.addEventListener('click', function(elem) {
+menuTag.forEach((elem) => {
+  elem.addEventListener('click', function () {
     menu.classList.add('none');
   });
 });
